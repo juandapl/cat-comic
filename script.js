e264b2afb21d87932df2c2df1d77915416dc4701
Juan Piñeros Liberato <74014859+juandapl@users.noreply.github.com>
2021-06-14 10:15:44 +0400

    added arrows and control bools

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ $(".arrow2").click(function () {
     });
 });
 
+$(".uparrow").click(function () {
+    window.scrollBy({
+        top: -window.innerHeight,
+        behavior: 'smooth'
+    });
+});
+
 function catWalk() { //function to animate cat walking
     var movingCatClass = document.getElementsByClassName("moving-cat-img"); //container with images
     var xVal = -80; //horizontal position of image
@@ -146,85 +153,99 @@ panel1.addEventListener("mouseover",showPanel1);
 panel2.addEventListener("mouseover",showPanel2);
 panel3.addEventListener("mouseover",showPanel3);
 
-
+panel1Shown = false;
 function showPanel1() {
-    var opacityVal=0.9;
-    var opacityVal2=-1.5;
-    var time = setInterval (frame, 100);
-    function frame() {
-        if (opacityVal==0) {
-            clearInterval(time);
+    if(!panel1Shown)
+    {
+        var opacityVal=0.9;
+        var opacityVal2=-1.5;
+        var time = setInterval (frame, 100);
+        function frame() {
+            if (opacityVal==0) {
+                clearInterval(time);
+            }
+            else {
+                overlay1.style.opacity=opacityVal;
+                opacityVal-=0.1;
+                document.getElementById("text1").style.opacity=opacityVal2;
+                document.getElementById("text2").style.opacity=opacityVal2-2;
+                opacityVal2+=0.1;
+            }
+        
         }
-        else {
-            overlay1.style.opacity=opacityVal;
-            opacityVal-=0.1;
-            document.getElementById("text1").style.opacity=opacityVal2;
-            document.getElementById("text2").style.opacity=opacityVal2-2;
+        panel1Shown = true;
+    }
+}
+
+panel2shown = false;
+function showPanel2() {
+    if(!panel2shown)
+    {
+        opacityVal=0.9;
+        opacityVal2=-1.5;
+        var time = setInterval (frame, 150);
+        function frame() {
+            overlay2.style.opacity=opacityVal;
+            opacityVal=opacityVal-0.1;
+            document.getElementById("text3").style.opacity=opacityVal2;
+            document.getElementById("text4").style.opacity=opacityVal2-1.5;
+            document.getElementById("text6").style.opacity=opacityVal2-3;
             opacityVal2+=0.1;
         }
-    
+        panel2shown = true;
+        catWalk();
+        
     }
-    
 }
 
-function showPanel2() {
-    opacityVal=0.9;
-    opacityVal2=-1.5;
-    var time = setInterval (frame, 150);
-    function frame() {
-        overlay2.style.opacity=opacityVal;
-        opacityVal=opacityVal-0.1;
-        document.getElementById("text3").style.opacity=opacityVal2;
-        document.getElementById("text4").style.opacity=opacityVal2-1.5;
-        document.getElementById("text6").style.opacity=opacityVal2-3;
-        opacityVal2+=0.1;
-    }
-    catWalk();
-}
-
+panel3shown = false;
 function showPanel3() {
-    opacityVal=0.9;
-    opacityVal2=-2.5;
-    var time = setInterval (frame, 150);
-    function frame() {
-        if (opacityVal2>=14) {
-            var yVal = 40;
-            var degreeTurn = 0;
-            clearInterval(time);
-            time=setInterval(frame2, 50);
-            function frame2() {
-                if (yVal>=600) {
-                    clearInterval(time);
-                }
-                else {
-                    y=yVal+"%";
-                    document.getElementById("falling-cat-img").style.opacity=1;
-                    document.getElementById("title3").style.opacity=0;
-                    overlay3.style.opacity=0.9;
-                    document.getElementById("falling-cat-img").style.top=y;
-                    window.scrollBy({
-                        top: window.innerHeight*2.5+75,
-                        behavior: 'smooth'
-                    });
-                    degree="rotate("+degreeTurn+"deg)";
-                    document.getElementById("falling-cat-img").style.transform=degree;
-                    degreeTurn+=45;
+    if(!panel3shown)
+    {
+        opacityVal=0.9;
+        opacityVal2=-2.5;
+        var time = setInterval (frame, 150);
+        function frame() {
+            if (opacityVal2>=14) {
+                var yVal = 40;
+                var degreeTurn = 0;
+                clearInterval(time);
+                time=setInterval(frame2, 50);
+                function frame2() {
+                    if (yVal>=600) {
+                        clearInterval(time);
+                    }
+                    else {
+                        y=yVal+"%";
+                        document.getElementById("falling-cat-img").style.opacity=1;
+                        document.getElementById("title3").style.opacity=0;
+                        overlay3.style.opacity=0.9;
+                        document.getElementById("falling-cat-img").style.top=y;
+                        window.scrollBy({
+                            top: window.innerHeight*2.5+75,
+                            behavior: 'smooth'
+                        });
+                        degree="rotate("+degreeTurn+"deg)";
+                        document.getElementById("falling-cat-img").style.transform=degree;
+                        degreeTurn+=45;
 
-                    yVal+=10;
+                        yVal+=10;
+                    }
                 }
             }
+            else {
+                overlay3.style.opacity=opacityVal;
+                opacityVal=opacityVal-0.1;
+                document.getElementById("text31").style.opacity=opacityVal2;
+                document.getElementById("text32").style.opacity=opacityVal2-3.75;
+                document.getElementById("text33").style.opacity=opacityVal2-7.75;
+                document.getElementById("text34").style.opacity=opacityVal2-11.25;
+                opacityVal2+=0.1;
+            }
         }
-        else {
-            overlay3.style.opacity=opacityVal;
-            opacityVal=opacityVal-0.1;
-            document.getElementById("text31").style.opacity=opacityVal2;
-            document.getElementById("text32").style.opacity=opacityVal2-3.75;
-            document.getElementById("text33").style.opacity=opacityVal2-7.75;
-            document.getElementById("text34").style.opacity=opacityVal2-11.25;
-            opacityVal2+=0.1;
-        }
+        panel3shown = true;
+        catWalk2();
     }
-    catWalk2();
 }
 
 function showPanel4() {}
